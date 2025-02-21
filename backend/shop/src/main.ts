eedc3d078e13cd12f3ed855fb8fc696012380568
gh0st1608 <egalindoa@uni.pe>
2025-02-21 02:30:02 -0500

    fix port shop

--- backend/shop/src/main.ts
+++ backend/shop/src/main.ts
@@ -5,8 +5,8 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(ShopModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 4001;
+  const port = configService.get<number>('PORT') || 5001;
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
