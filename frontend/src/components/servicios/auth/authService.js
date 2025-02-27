82230bde6a3cb80a14b0a5d8efba8d9bcd0c353c
va314 <lospemas@gmail.com>
2025-02-26 19:17:34 -0500

    update

--- frontend/src/components/servicios/auth/authService.js
+++ frontend/src/components/servicios/auth/authService.js
@@ -4,17 +4,6 @@ import { ENDPOINTS } from "../endpoints";
 // ✅ Iniciar Sesión
 export const loginUser = async (credentials) => {
   try {
-<<<<<<< HEAD:frontend/src/components/servicios/authService.js
-    console.log("📡 Sending login request...", credentials);
-    
-    const response = await api.post(ENDPOINTS.auth.login, credentials);
-    console.log("✅ Login successful:", response.data);
-
-    // Guardar el token en localStorage
-    localStorage.setItem("token", response.data.token);
-    
-    return response.data;
-=======
     console.log("📡 Sending login request...");
     const response = await postRequest(ENDPOINTS.auth.login, credentials);
     console.log("✅ Login successful:", response.accessToken);
@@ -22,7 +11,6 @@ export const loginUser = async (credentials) => {
     localStorage.setItem("token", response.accessToken);
 
     return response.accessToken;
->>>>>>> a6e7cba12553a87c4f50d03d7750ff1a42748671:frontend/src/components/servicios/auth/authService.js
   } catch (error) {
     console.error("❌ Login failed:", error.response ? error.response.data : error.message);
     return null;
@@ -32,14 +20,8 @@ export const loginUser = async (credentials) => {
 // ✅ Registrar Usuario
 export const registerUser = async (userData) => {
   try {
-<<<<<<< HEAD:frontend/src/components/servicios/authService.js
-    console.log("📡 Sending registration request...", userData);
-    
-    const response = await api.post(ENDPOINTS.auth.register, userData);
-=======
     console.log("📡 Sending registration request...");
     const response = await postRequest(ENDPOINTS.auth.register, userData);
->>>>>>> a6e7cba12553a87c4f50d03d7750ff1a42748671:frontend/src/components/servicios/auth/authService.js
     console.log("✅ Registration successful:", response.data);
     return response.data;
   } catch (error) {
@@ -53,4 +35,3 @@ export const logoutUser = () => {
   console.log("🔐 Logging out...");
   localStorage.removeItem("token");
 };
-// dd
