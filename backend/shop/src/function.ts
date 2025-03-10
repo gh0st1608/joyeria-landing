f26e19223a9f4841e81fad43be5d2fec18f0eaf6
gh0st1608 <egalindoa@uni.pe>
2025-03-10 02:02:11 -0500

    add shop function

---
+++ backend/shop/src/function.ts
@@ -0,0 +1,25 @@
+import { ShopModule } from './shop.module';
+import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import * as functions from '@google-cloud/functions-framework';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap(): Promise<express.Express> {
+  const expressApp = express();
+  const app = await NestFactory.create(ShopModule, new ExpressAdapter(expressApp));
+  //const configService = app.get(ConfigService);
+  //const port = configService.get<number>('PORT') || 5001;
+  app.setGlobalPrefix('shop');
+  app.enableCors();
+  
+  await app.init(); // Esperar la inicialización completa
+  return expressApp; // Regresar la app ya inicializada
+}
+
+// Exporta la función de Cloud Function con nombre
+export const shopHandler = functions.http('shopHandler', async (req, res) => {
+  // Asegúrate de que la app esté completamente inicializada antes de manejar la solicitud
+  const expressApp = await bootstrap(); // Esta espera que NestJS esté listo
+  expressApp(req, res); // Procesa la solicitud HTTP con Express
+});
