ea3b9ff3993cf28c369ea8bea92ba1d0f176ac37
va314 <lospemas@gmail.com>
2025-03-27 16:26:45 -0500

    update

--- frontend/src/components/servicios/dashboard/purchaseService.js
+++ frontend/src/components/servicios/dashboard/purchaseService.js
@@ -1,18 +1,19 @@
 import { getRequest, postRequest, putRequest, deleteRequest } from "./api";
 import { ENDPOINTS } from "../../servicios/endpoints";
 
+// Obteniendo compras desde /cartbuy
 export const getPurchases = async () => {
-  return await getRequest(ENDPOINTS.dashboard.purchases);
+  return await getRequest(ENDPOINTS.payment.cart);
 };
 
 export const createPurchase = async (data) => {
-  return await postRequest(ENDPOINTS.dashboard.createPurchase, data);
+  return await postRequest(ENDPOINTS.payment.create, data);
 };
 
 export const updatePurchase = async (id, data) => {
-  return await putRequest(ENDPOINTS.dashboard.updatePurchase(id), data);
+  return await putRequest(ENDPOINTS.payment.getCartById(id), data);
 };
 
 export const deletePurchase = async (id) => {
-  return await deleteRequest(ENDPOINTS.dashboard.deletePurchase(id));
+  return await deleteRequest(ENDPOINTS.payment.getCartById(id));
 };
